1e8efc05a18bef4785ab4d437c824df24fc27036
Peter Keogh <keoghpe@tcd.ie>
2014-03-13 13:42:21 +0800

    Cleaned up code and removed unnecessary files

---
+++ README.md
@@ -0,0 +1,20 @@
+/------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-------------------/
+
+THIS ISN'T GONNA WORK IF YOU HAVE JAVASCRIPT DISABLED.
+THIS ISN'T THE 90'S. IF YOU'RE USING A SHITTY BROWSER THIS WON'T WORK.
+
+/------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-------------------/
+
+Here's how to use the application.
+
+You can view and black box test the application at this url:
+
+fresa867.github.io
+
+To download the source code visit:
+
+github.com/fresa867/fresa867.github.io
+
+After downloading the source code you can run it by opening the index.html file in your browser.
+
+Enjoy!
--- js/controllers/DateController.js
+++ js/controllers/DateController.js
@@ -1,25 +1,23 @@
 var dateApp = angular.module('DateApp', []);
 var monthStrings = ['January','February','March',
-					'April','May','June',
-					'July','August', 'September',
-					'October','November','December'];
+'April','May','June',
+'July','August', 'September',
+'October','November','December'];
 
 function DateCtrl ($scope) {
 	var daysInMonth = 31;
 	var chineseCharacter = '';
 
 	$scope.mdate = {
-		year: 2013,
+		year: "2013",
 		month: 12,
 		day: 31
 	};
 
 	$scope.getTomorrow = function (year, month, day) {
-		//var tomorrow = $scope.date.day;
-		//return $scope.mdate;
+
 		var Tomorrow = $scope.calculateTomorrow(year,month,day);
-		console.log(Tomorrow);
-		// 
+		
 		if ($scope.checkDay(day) === "error" || $scope.checkMonth(month) === "error" 
 			|| $scope.checkYear(year) === "error" || $scope.enterDate.year.$error.minlength 
 			|| $scope.enterDate.year.$error.pattern || $scope.enterDate.month.$error.pattern
@@ -69,10 +67,10 @@ function DateCtrl ($scope) {
 
 		if (month === 2) {
 			if ($scope.isLeapYear(year)) {
-					daysInMonth = 29;
-				} else{
-					daysInMonth = 28;
-				}
+				daysInMonth = 29;
+			} else{
+				daysInMonth = 28;
+			}
 		} else if (month === 4 || month === 6 || month === 9 || month === 11) {
 			daysInMonth = 30;
 		} else {
@@ -92,7 +90,6 @@ function DateCtrl ($scope) {
 		{
 			tomorrow.Day++;
 		}
-
 		return tomorrow;
 	};
 
@@ -125,51 +122,46 @@ function DateCtrl ($scope) {
 		return daysInMonth;
 	};
 
-	$scope.getDayOfWeek = function(year, month, day) { // Function getting the next day
+	$scope.getDayOfWeek = function(year, month, day) { 
     // Algorithm from http://calendars.wikia.com/wiki/Calculating_the_day_of_the_week
 
-    var dayNames = new Array("Sunday","Monday", "Tuesday", "Wednesday",
-    "Thursday", "Friday", "Saturday");
+		var dayNames = new Array("Sunday","Monday", "Tuesday", "Wednesday",
+			"Thursday", "Friday", "Saturday");
 
-    var monthDayNumbersNoLeap = new Array(0,3,3,6,1,4,6,2,5,0,3,5);
+		var monthDayNumbersNoLeap = new Array(0,3,3,6,1,4,6,2,5,0,3,5);
 
-    var monthDayNumbersWithLeap = new Array(6,2,3,6,1,4,6,2,5,0,3,5);
-   
-    var century = parseInt(year.toString().substr(0, 2));
-    
-    var yearNumber = parseInt(year.toString().substr(2, 3));
+		var monthDayNumbersWithLeap = new Array(6,2,3,6,1,4,6,2,5,0,3,5);
 
-    var centuryNumber = (3-(century%4))*2;
+		var century = parseInt(year.toString().substr(0, 2));
 
-    var numberOfLeapYears = Math.floor(yearNumber/4);
+		var yearNumber = parseInt(year.toString().substr(2, 3));
 
-    var monthNumber = 0;
+		var centuryNumber = (3-(century%4))*2;
 
-    if($scope.isLeapYear(year)) {
-     monthNumber = monthDayNumbersWithLeap[month-1];
-    } else {
-     monthNumber = monthDayNumbersNoLeap[month-1];
-    }
+		var numberOfLeapYears = Math.floor(yearNumber/4);
 
-    var sumOfCenturyYearMonthAndDay = centuryNumber+yearNumber+numberOfLeapYears+monthNumber+day;
+		var monthNumber = 0;
 
-    var dayOfWeekNumber = sumOfCenturyYearMonthAndDay%7;
+		if($scope.isLeapYear(year)) {
+			monthNumber = monthDayNumbersWithLeap[month-1];
+		} else {
+			monthNumber = monthDayNumbersNoLeap[month-1];
+		}
 
-    var dayOfWeek = dayNames[dayOfWeekNumber];
+		var sumOfCenturyYearMonthAndDay = centuryNumber+yearNumber+numberOfLeapYears+monthNumber+day;
 
-	return dayOfWeek;
-    };
+		var dayOfWeekNumber = sumOfCenturyYearMonthAndDay%7;
 
-    $scope.getChineseCalendarDate = function(theYear){
+		var dayOfWeek = dayNames[dayOfWeekNumber];
+
+		return dayOfWeek;
+	};
+
+	$scope.getChineseCalendarDate = function(theYear){
 
 		//Algorithm from http://www.hermetic.ch/cal_stud/ch_year.htm
 
-		//var i = 1983%2 ? (1983+6)%10  - 1: (1983+6)%10;
-
-		///var i = theYear%2 ? (theYear+6)%10  - 1: (theYear+6)%10;
-
 		theYear = parseInt(theYear);
-		console.log(theYear);
 
 		var i = 0;
 		var a = 0;
@@ -178,33 +170,18 @@ function DateCtrl ($scope) {
 		if (theYear%2 === 0) {
 			i = theYear + 6;
 			i = i%10;
-			console.log('I is '+i);
 		} else{
 			i = theYear + 6;
 			i = i%10;
-			i--;
-			console.log('I is '+i);		
+			i--;	
 		}
-
 
 		e = i/2;
 		a = theYear +8;
 		a = a % 12;
 
-		//var k = 0;
-
-		/*while(60*k < theYear + 2756)
-		{
-			k++;
-		}*/
-
 		var element = ['Wood','Fire','Earth','Metal','Water'];
 		var animal = ['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Sheep', 'Monkey', 'Rooster' ,'Dog','Pig'];
-		//var animalChars = ["鼠"，"牛"，"虎"，"兔"，"龍"，"蛇"，"馬"，"羊"，"猴子"，"網格"，"狗"，"豬"];
-		//chineseCharacter = animalChars[a];
-		//chineseCharacter = 'hello';
-
-		console.log('A is ' + a+ '\n E is' + e);
 
 		var animalChars = [];
 		animalChars[0] = {value:"鼠"};
@@ -240,5 +217,4 @@ function DateCtrl ($scope) {
 			return "autumn";
 		}
 	};
-
 }
